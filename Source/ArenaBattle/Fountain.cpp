b212d352cce6167b01d4f468bc1ea19a601e0a19
merry0729 <merry0729@naver.com>
2024-07-19 23:21:23 +0900

    240719 Update
    - 액터 회전
    - 로그 출력

--- Source/ArenaBattle/Fountain.cpp
+++ Source/ArenaBattle/Fountain.cpp
@@ -23,14 +23,37 @@ AFountain::AFountain()
 	Light->SetRelativeLocation(FVector(0.0f, 0.0f, 195.0f));
 	Splash->SetRelativeLocation(FVector(0.0f, 0.0f, 135.0f));
 
-	ConstructorHelpers::FObjectFinder<UStaticMesh>
-	SM_BODY(TEXT("/Game/InfinityBladeGrassLands/Environments/Plains/Env_Plains_Ruins/StaticMesh/SM_Plains_Castle_Fountain_01.SM_Plains_Castle_Fountain_01"));
+	static ConstructorHelpers::FObjectFinder<UStaticMesh>
+		SM_BODY(TEXT("/Game/InfinityBladeGrassLands/Environments/Plains/Env_Plains_Ruins/StaticMesh/SM_Plains_Castle_Fountain_01.SM_Plains_Castle_Fountain_01"));
 
-	if (SM_BODY.Succeeded)
+	if (SM_BODY.Succeeded())
 	{
 		Body->SetStaticMesh(SM_BODY.Object);
 	}
+
+	static ConstructorHelpers::FObjectFinder<UStaticMesh>
+		SM_WATER(TEXT("/Game/InfinityBladeGrassLands/Environments/Plains/Env_Plains_Ruins/StaticMesh/SM_Plains_Castle_Fountain_02.SM_Plains_Castle_Fountain_02"));
+
+	if (SM_WATER.Succeeded())
+	{
+		Body->SetStaticMesh(SM_WATER.Object);
+	}
+
+	static ConstructorHelpers::FObjectFinder<UParticleSystem>
+		PS_SPLASH(TEXT("/Game/InfinityBladeGrassLands/Effects/FX_Ambient/Water/P_Water_Fountain_Splash_Base_01.P_Water_Fountain_Splash_Base_01"));
+
+	if (PS_SPLASH.Succeeded())
+	{
+		Splash->SetTemplate(PS_SPLASH.Object);
+	}
 	
+	RotateSpeed = 30.0f;
+}
+
+void AFountain::PostInitializeComponents()
+{
+	Super::PostInitializeComponents();
+	ABLOG_S(Warning);
 }
 
 // Called when the game starts or when spawned
@@ -38,12 +61,21 @@ void AFountain::BeginPlay()
 {
 	Super::BeginPlay();
 	
+	//UE_LOG(ArenaBattle, Warning, TEXT("Actor Name : %s, ID : %d, Location X : %.3f"), *GetName(), ID, GetActorLocation().X);
+
+	ABLOG_S(Warning);
+	ABLOG(Warning, TEXT("Actor Name : %s, ID : %d, Location X : %.3f"), *GetName(), ID, GetActorLocation().X);
 }
 
 // Called every frame
 void AFountain::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-
+	AddActorLocalRotation(FRotator(0.0f, RotateSpeed * DeltaTime, 0.0f));
 }
 
+void AFountain::EndPlay(const EEndPlayReason::Type EndPlayReason)
+{
+	Super::EndPlay(EndPlayReason);
+	ABLOG_S(Warning);
+}
--- Source/ArenaBattle/Fountain.h
+++ Source/ArenaBattle/Fountain.h
@@ -2,7 +2,7 @@
 
 #pragma once
 
-#include "EngineMinimal.h"
+#include "ArenaBattle.h"
 #include "GameFramework/Actor.h"
 #include "Fountain.generated.h"
 
@@ -18,6 +18,8 @@ public:
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+	virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
+	virtual void PostInitializeComponents() override;
 
 public:	
 	// Called every frame
@@ -37,4 +39,8 @@ public:
 
 	UPROPERTY(EditAnywhere, Category=ID)
 	int32 ID;
+
+private:
+	UPROPERTY(EditAnywhere, Category=Stat, Meta = (AllowPrivateAccess = true))
+	float RotateSpeed;
 };
