3631c7667d3def791d717349083c70ecd65f9de9
merry0729 <merry0729@naver.com>
2024-08-10 00:16:58 +0900

    240810 Update
    - 디버그 드로잉

--- Source/ArenaBattle/ABCharacter.cpp
+++ Source/ArenaBattle/ABCharacter.cpp
@@ -274,36 +274,37 @@ void AABCharacter::AttackCheck()
 	bool bResult = GetWorld()->SweepSingleByChannel(
 		HitResult,
 		GetActorLocation(),
-		GetActorLocation() + GetActorForwardVector() * 200.0f,
-		FQuat::Identity,
-		ECollisionChannel::ECC_GameTraceChannel2,
-		FCollisionShape::MakeSphere(50.0f),
-		Params);
-
-	if (bResult)
-	{
-		ABLOG(Warning, TEXT("Hit Actor Name"));
-
-
-		if (HitResult.Actor.IsValid())
-		{
-			ABLOG(Warning, TEXT("Hit Actor Name : %s"), *HitResult.Actor->GetName());
-		}
-	}
-}
-
-void AABCharacter::AttackCheck()
-{
-	FHitResult HitResult;
-	FCollisionQueryParams Params(NAME_None, false, this);
-	bool bResult = GetWorld()->SweepSingleByChannel(
-		HitResult,
-		GetActorLocation(),
 		GetActorLocation() + GetActorForwardVector() + AttackRange,
 		FQuat::Identity,
 		ECollisionChannel::ECC_GameTraceChannel2,
 		FCollisionShape::MakeSphere(AttackRadius),
 		Params);
 
+#if ENABLE_DRAW_DEBUG
 
+	FVector TraceVec = GetActorForwardVector() * AttackRange;
+	FVector Center = GetActorLocation() + TraceVec * 0.5f;
+	float HalfHeight = AttackRange * 0.5f + AttackRadius;
+	FQuat CapsuleRot = FRotationMatrix::MakeFromZ(TraceVec).ToQuat();
+	FColor DrawColor = bResult ? FColor::Green : FColor::Red;
+	float DebugLifeTime = 5.0;
+
+	DrawDebugCapsule(GetWorld(),
+	Center,
+	HalfHeight,
+	AttackRadius,
+	CapsuleRot,
+	DrawColor,
+	false,
+	DebugLifeTime);
+
+#endif
+
+	if (bResult)
+	{
+		if (HitResult.Actor.IsValid())
+		{
+			ABLOG(Warning, TEXT("Hit Actor Name : %s"), *HitResult.Actor->GetName());
+		}
+	}
 }
