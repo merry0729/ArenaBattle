54da8b751e5bb0a2f93a239592c1b34e0e0a256b
merry0729 <merry0729@naver.com>
2024-08-28 15:36:58 +0900

    240828 Update
    - Nav AIControl

--- Source/ArenaBattle/ABAIController.cpp
+++ Source/ArenaBattle/ABAIController.cpp
@@ -2,6 +2,36 @@
 
 #include "ABAIController.h"
 
+AABAIController::AABAIController()
+{
+	RepeatInterval = 3.0f;
+}
 
+void AABAIController::Possess(APawn* InPawn)
+{
+	Super::Possess(InPawn);
+	GetWorld()->GetTimerManager().SetTimer(RepeatTimerHandle, this, &AABAIController::OnRepeatTimer, RepeatInterval, true);
+}
 
+void AABAIController::UnPossess()
+{
+	Super::UnPossess();
+	GetWorld()->GetTimerManager().ClearTimer(RepeatTimerHandle);
+}
+
+void AABAIController::OnRepeatTimer()
+{
+	auto CurrentPawn = GetPawn();
+	ABCHECK(nullptr != CurrentPawn);
+
+	UNavigationSystem* NavSystem = UNavigationSystem::GetNavigationSystem(GetWorld());
+	if(nullptr == NavSystem) return;
+
+	FNavLocation NextLocation;
+	if(NavSystem->GetRandomPointInNavigableRadius(FVector::ZeroVector, 500.0f, NextLocation))
+	{
+		UNavigationSystem::SimpleMoveToLocation(this, NextLocation.Location);
+		ABLOG(Warning, TEXT("Next Location : %s"), *NextLocation.Location.ToString());
+	}
+}
 
