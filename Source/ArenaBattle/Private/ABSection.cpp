b49b9b200fa27a04a6be8ceb8a84bc50135a44e5
merry0729 <merry0729@naver.com>
2024-09-12 00:01:33 +0900

    240912 Update
    - Create Infinity Map

---
+++ Source/ArenaBattle/Private/ABSection.cpp
@@ -0,0 +1,52 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "ABSection.h"
+
+
+// Sets default values
+AABSection::AABSection()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+
+	Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MESH"));
+	RootComponent = Mesh;
+
+	FString AssetPath = TEXT("/Game/Book/StaticMesh/SM_SQUARE.SM_SQUARE");
+	static ConstructorHelpers::FObjectFinder<UStaticMesh> SM_SQUARE(*AssetPath);
+	if(SM_SQUARE.Succeeded())
+	{
+		Mesh->SetStaticMesh(SM_SQUARE.Object);
+	}
+	else
+	{
+		ABLOG(Error, TEXT("Failed to load staticmesh asset. : %s"), *AssetPath);
+	}
+
+	FString GateAssetPath = TEXT("/Game/Book/StaticMesh/SM_GATE.SM_GATE");
+	static ConstructorHelpers::FObjectFinder<UStaticMesh> SM_GATE(*GateAssetPath);
+	if (!SM_GATE.Succeeded())
+	{
+		ABLOG(Error, TEXT("Failed to load staticmesh asset. : %s"), *GateAssetPath);
+	}
+
+	static FName GateSockets[] = { { TEXT("+XGate") }, { TEXT("-Gate") }, {TEXT("+YGate") }, {TEXT("-Gate") }};
+	for (FName GateSocket : GateSockets)
+	{
+		ABCHECK(Mesh->DoesSocketExist(GateSocket));
+		UStaticMeshComponent* NewGate = CreateDefaultSubobject<UStaticMeshComponent>(*GateSocket.ToString());
+		NewGate->SetStaticMesh(SM_GATE.Object);
+		NewGate->SetupAttachment(RootComponent, GateSocket);
+		NewGate->SetRelativeLocation(FVector(0.0f, -80.0f, 0.0f));
+		GateMeshes.Add(NewGate);
+	}
+}
+
+// Called when the game starts or when spawned
+void AABSection::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+
