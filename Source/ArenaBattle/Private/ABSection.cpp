faf7d8223cf8564c9f452d17511dcf559e4de2e3
merry0729 <merry0729@naver.com>
2024-09-21 22:11:49 +0900

    240921 Update
    - Navi Mesh System Setting

--- Source/ArenaBattle/Private/ABSection.cpp
+++ Source/ArenaBattle/Private/ABSection.cpp
@@ -1,7 +1,8 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "ABSection.h"
-
+#include "ABCharacter.h"
+#include "ABItemBox.h"
 
 // Sets default values
 AABSection::AABSection()
@@ -60,6 +61,9 @@ AABSection::AABSection()
 	}
 
 	bNoBattle = false;
+
+	EnemySpawnTime = 2.0f;
+	ItemBoxSpawnTime = 5.0f;
 }
 
 // Called when the game starts or when spawned
@@ -94,6 +98,14 @@ void AABSection::SetState(ESectionState NewState)
 			}
 
 			OperateGates(false);
+
+			GetWorld()->GetTimerManager().SetTimer(SpawnNPCTimerHandle, FTimerDelegate::CreateUObject(this, &AABSection::OnNPCSpawn), EnemySpawnTime, false);
+
+			GetWorld()->GetTimerManager().SetTimer(SpawnItemBoxTimerHandle, FTimerDelegate::CreateLambda([this]() -> void {
+				FVector2D RandXY = FMath::RandPointInCircle(600.0f);
+				GetWorld()->SpawnActor<AABItemBox>(GetActorLocation() + FVector(RandXY, 30.0f), FRotator::ZeroRotator);
+				}), ItemBoxSpawnTime, false);
+
 			break;
 		}
 		case ESectionState::COMPLETE:
@@ -165,4 +177,9 @@ void AABSection::OnGateTriggerBeginOverlap(UPrimitiveComponent* OverlappedCompon
 	{
 		ABLOG(Warning, TEXT("New section area is not empty"));
 	}
+}
+
+void AABSection::OnNPCSpawn()
+{
+	GetWorld()->SpawnActor<AABCharacter>(GetActorLocation() + FVector::UpVector * 80.0f, FRotator::ZeroRotator);
 }
